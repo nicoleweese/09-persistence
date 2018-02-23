dbf44c14c70580100ea2b3e436e5c5022b0f17c2
nicoleweese <nweese@ymail.com>
2018-02-22 22:28:10 -0800

    woo

--- lab-nicole/server.js
+++ lab-nicole/server.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const http = require('http');
-const Doggo = require('./model/doggo.js');
 const Router = require('./lib/router.js');
 const doggoRouter = require('./route/doggo-route.js');
-const storage = require('./lib/storage.js');
 const PORT = process.env.PORT || 3000;
 const router = new Router();
 
